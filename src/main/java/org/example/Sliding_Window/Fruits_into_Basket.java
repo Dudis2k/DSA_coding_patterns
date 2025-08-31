97b974b7dc86556762e9ed58374e77c876fe3e36
Siddu Gowda <82716082+Dudis2k@users.noreply.github.com>
2025-08-31 12:09:00 +0530

    2. Update Fruits_into_Basket.java

--- src/main/java/org/example/Sliding_Window/Fruits_into_Basket.java
+++ src/main/java/org/example/Sliding_Window/Fruits_into_Basket.java
@@ -3,35 +3,51 @@ package org.example.Sliding_Window;
 import java.util.*;
 
 public class Fruits_into_Basket {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int n;
-        System.out.println("Enter the size of the array:");
-        n = sc.nextInt();
-        char[] input_arr = new char[n];
-        System.out.println("Enter elements :");
-        for(int i = 0; i < n; i++) {
-            input_arr[i] = sc.next().charAt(0);
-        }
-        System.out.println("Maximum number of fruits that can be collected is " + maxFruits(input_arr));
+    private char[] Fruits;
+    private int len;
+    private Scanner scanner;
+    public Fruits_into_Basket(){
+        scanner = new Scanner(System.in);
     }
-    public static int maxFruits(char[] fruits) {
-        Map<Character, Integer> basket = new HashMap<>();
-        int i = 0, maxCount = Integer.MIN_VALUE, count = 0, start = 0;
-        while(i<fruits.length) {
-            basket.put(fruits[i], basket.getOrDefault(fruits[i], 0) + 1);
-            count++;
-            while(basket.size()>2){
-                basket.put(fruits[start], basket.get(fruits[start]) - 1);
-                if(basket.get(fruits[start]) == 0) {
-                    basket.remove(fruits[start]);
-                }
-                start++;
-                count--;
-            }
-            i++;
-            maxCount = Math.max(maxCount, count);
+
+    public void take_inputs(){
+        System.out.println("Enter the Character Array Size: ");
+        len = scanner.nextInt();
+        Fruits = new char[len];
+        System.out.println("Characters are! - ");
+        for(int i=0;i<len;i++){
+            Fruits[i] = scanner.next().charAt(0);
         }
-        return maxCount;
+    }
+    //This method is to obtain the max number of fruits count in a basket
+    // if basket can fit any number of fruits but of only 2 types
+    private int get_max_number_of_fruits_in_basket(){
+        StringBuilder stringBuilder = new StringBuilder();
+        Map<Character, Integer> fruit_varieties = new HashMap<>();
+        int start =0, sum =0, out =Integer.MIN_VALUE, i=0;
+        while(i<Fruits.length){
+            if(fruit_varieties.size()<2 || fruit_varieties.containsKey(Fruits[i])) {
+                fruit_varieties.put(Fruits[i],fruit_varieties.getOrDefault(Fruits[i],0)+1);
+                sum++;
+            }
+            else{
+                while(fruit_varieties.size()>1){
+                    fruit_varieties.put(Fruits[start],fruit_varieties.getOrDefault(Fruits[start],0)-1);
+                    if(fruit_varieties.get(Fruits[start])==0){
+                        fruit_varieties.remove(Fruits[start]);
+                    }
+                    sum--;
+                    start++;
+                }
+                i--;
+            }
+            out = Math.max(out, sum);
+            i++;
+        }
+        return out;
+    }
+
+    public void get_results(){
+        System.out.println("Max Fruits that can be in basket are = " + get_max_number_of_fruits_in_basket());
     }
 }
